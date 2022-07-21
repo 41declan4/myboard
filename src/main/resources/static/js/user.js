5bdcdc0ad4ddeaf6278e403b8219806bff0b151d
Shin Dong Yeol <41declan4@gmail.com>
2022-07-21 22:16:30 +0900

    회원가입 주소 에러 변경
    회원가입 주소 에러 변경 Javascript

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -20,11 +20,16 @@ let index = {
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'
         }).done(function(resp) {
-            alert('회원가입이 완료되었습니다.');
-            location.href = '/';
+            if (resp === 500) {
+                alert('회원가입 실패');    
+            } else {
+                alert('회원가입 완료');
+                location.href = '/';
+            }
             
         }).fail(function(error) {
-            alert(JSON.stringify(error));
+            alert("회원가입 실패");
+            console.log(error);
         })
     }
 }
